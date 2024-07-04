e7a46b07399dd264324c62cdf34e2b94b1f70c36
jakepeg <jakepeg@gmail.com>
2024-07-04 21:44:55 +0100

    Fix headers

--- src/canapi_app_frontend/src/screens/Canisters.jsx
+++ src/canapi_app_frontend/src/screens/Canisters.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Canisters() {
   return (
-    <div>Canisters</div>
+    <div className="content-center">
+      <h1 className="m-6 text-2xl text-center">Canisters</h1>
+    </div>
   )
 }
--- src/canapi_app_frontend/src/screens/Marketplace.jsx
+++ src/canapi_app_frontend/src/screens/Marketplace.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Marketplace() {
   return (
-    <div>Marketplace</div>
+    <div className="content-center">
+      <h1 className="m-6 text-2xl text-center">Marketplace</h1>
+    </div>
   )
 }
